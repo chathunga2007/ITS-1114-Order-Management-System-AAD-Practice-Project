e67e3f4ca852382fefec3e10fd046810236b7041
Chathunga Bimsara <wggachathungabimsara2007@gmail.com>
2026-06-17 12:05:16 +0530

    item repository updated

--- src/main/java/lk/ijse/OrderManagementSystem/repository/ItemRepository.java
+++ src/main/java/lk/ijse/OrderManagementSystem/repository/ItemRepository.java
@@ -2,8 +2,16 @@ package lk.ijse.OrderManagementSystem.repository;
 
 import lk.ijse.OrderManagementSystem.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,Long> {
+    @Query(value = """
+    SELECT *
+    FROM item i
+    WHERE (?1 IS NULL OR i.item_name LIKE CONCAT('%', ?1, '%'))
+    """, nativeQuery = true)
+    List<Item> filterItems(String itemName);
 }
